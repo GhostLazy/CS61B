77b0e3be0a95d61dd77c891825dc373f358b75df
GhostLazy <2265041410@qq.com>
2024-11-26 14:41:13 +0800

    check

--- hw1/synthesizer/GuitarString.java
+++ hw1/synthesizer/GuitarString.java
@@ -1,4 +1,3 @@
-// TODO: Make sure to make this class a part of the synthesizer package
 package synthesizer;
 
 //Make sure this class is public
@@ -14,10 +13,6 @@ public class GuitarString {
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
-        // TODO: Create a buffer with capacity = SR / frequency. You'll need to
-        //       cast the result of this division operation into an int. For better
-        //       accuracy, use the Math.round() function before casting.
-        //       Your buffer should be initially filled with zeros.
         buffer = new ArrayRingBuffer<>((int) Math.round(SR / frequency));
         for (int i = 0; i < buffer.capacity(); i++) {
             buffer.enqueue(0.0);
@@ -27,29 +22,25 @@ public class GuitarString {
 
     /* Pluck the guitar string by replacing the buffer with white noise. */
     public void pluck() {
-        // TODO: Dequeue everything in the buffer, and replace it with random numbers
-        //       between -0.5 and 0.5. You can get such a number by using:
-        //       double r = Math.random() - 0.5;
-        //
-        //       Make sure that your random numbers are different from each other.
-    }
-
-    /* Advance the simulation one time step by performing one iteration of
-     * the Karplus-Strong algorithm. 
-     */
-    public void tic() {
-        // TODO: Dequeue the front sample and enqueue a new sample that is
-        //       the average of the two multiplied by the DECAY factor.
-        //       Do not call StdAudio.play().
         for (int i = 0; i < buffer.capacity(); i++) {
             buffer.dequeue();
             buffer.enqueue(Math.random() - 0.5);
         }
     }
 
+    /* Advance the simulation one time step by performing one iteration of
+     * the Karplus-Strong algorithm. 
+     */
+    public void tic() {
+        for (int i = 0; i < buffer.capacity(); i++) {
+            double x = buffer.dequeue();
+            double y = buffer.peek();
+            buffer.enqueue(0.5 * (x + y));
+        }
+    }
+
     /* Return the double at the front of the buffer. */
     public double sample() {
-        // TODO: Return the correct thing.
         double x = buffer.dequeue();
         buffer.enqueue(x * DECAY);
         return 0;
